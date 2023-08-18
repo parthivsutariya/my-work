77c228c6bbec72c5335d68b5a607c7a24dc14b89
parthivsutariya <140811723+parthivsutariya@users.noreply.github.com>
2023-08-18 10:38:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+import { MainNavigation } from "./components/MainNavigation";
+import loader from "../src/assets/images/loader.gif";
+import { Footer } from "./components/footer";
+import { AuthWrapper } from "./context/auth";
+import Header from "./components/header";
+import { CartWrapper } from "./context/cart";
 
-function App() {
-  let x = 10
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React application {x}.
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthWrapper>
+        <CartWrapper>
+          <ThemeProvider theme={theme}>
+            <div className="loader-wrapper">
+              <img src={loader} alt="loader" />
+            </div>
+            <Header />
+            <main>
+              <MainNavigation />
+            </main>
+            <Footer />
+            <ToastContainer />
+          </ThemeProvider>
+        </CartWrapper>
+      </AuthWrapper>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
